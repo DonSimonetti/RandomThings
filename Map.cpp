2dc962b97f28bd37174f8cba9e4e23ecacdbe9db
DonSimonetti <matt.simonetti@hotmail.it>
2018-09-04 18:56:30 +0200

    Adding streamer compatibility

--- Map.cpp
+++ Map.cpp
@@ -28,7 +28,7 @@ void Map::loadMap()
     string mapdir(getCurrentDirectory()+"/scriptfiles/Maps/"+name);
     if(fileExists(mapdir))
     {
-        logprintf("  [RandomThings] Loading Map \" %s \" ...",name.c_str());
+        logprintf("[RandomThings] Loading Map \" %s \" ...",name.c_str());
 
         std::ifstream mapfile;
         mapfile.open(mapdir.c_str());
@@ -38,24 +38,24 @@ void Map::loadMap()
             int objNumber=0;
             while(getline(mapfile,line))
             {
-                if(line.find("CreateObject(")!=std::string::npos) {
-
-                    objNumber++;
-                    loadStandardObject(line,objNumber);
-                }
-                else if(line.find("CreateObject(")!=std::string::npos)
+                if(line.find("CreateObject(")!=std::string::npos)
                 {
+                    loadStandardObject(line,objNumber);
                     objNumber++;
-                    loadDynamicObject(line, objNumber);
+                }
+                else if(line.find("CreateDynamicObject(")!=std::string::npos)
+                {
+                    //loadDynamicObject(line, objNumber);
+                    objNumber++;
                 }
             }
             mapfile.close();
 
-            logprintf("  [RandomThings] Loading Completed!");
+            logprintf("[RandomThings] Loading Completed!");
         }
     }
     else
-        logprintf("  [RandomThings] Requested loading Map \" %s \" but file doesn't exist. Skipped.",mapdir.c_str());
+        logprintf("[RandomThings] Requested loading Map \" %s \" but file doesn't exist. Skipped.",mapdir.c_str());
 }
 
 void Map::loadStandardObject(string line, int index)
@@ -63,33 +63,49 @@ void Map::loadStandardObject(string line, int index)
     line.erase(0, line.find("CreateObject(") + string("CreateObject(").length());
     line.erase(line.find_last_of(")"), line.length());
 
-    unsigned int modelid;
+    int modelid;
     float posX, posY, posZ, rotX, rotY, rotZ;
-    int succCode = sscanf(line.c_str(), "%u,%f,%f,%f,%f,%f,%f", &modelid, &posX, &posY, &posZ, &rotX, &rotY, &rotZ);
+    int succCode = sscanf(line.c_str(), "%d,%f,%f,%f,%f,%f,%f", &modelid, &posX, &posY, &posZ, &rotX, &rotY, &rotZ);
 
-    if (succCode == 7) {
-        int obj = CreateObject(modelid, posX, posY, posZ, rotX, rotY, rotZ, drawDistance);
+    if (succCode == 7)
+    {
+        int obj;
+        AMX_NATIVE native=sampgdk_FindNative("CreateDynamicObject");
+        if(native!=NULL)
+        {
+            obj=sampgdk_InvokeNative(native,"dffffffdddffdd",modelid,posX,posY,posZ,rotX,rotY,rotZ,-1,0,-1,drawDistance,drawDistance,-1,0);
+        }
+        else
+        {
+            obj = CreateObject(modelid, posX, posY, posZ, rotX, rotY, rotZ, drawDistance);
+        }
         objects.push_back(obj);
     }
     else
-        logprintf("  Error: Object %d has wrong format",index);
+        logprintf("Error: Object %d has wrong format",index);
 }
 
 void Map::loadDynamicObject(std::string line, int index)
 {
-    line.erase(0, line.find("CreateDynamicObject(") + string("CreateDynamicObject(").length());
+    /*line.erase(0, line.find("CreateDynamicObject(") + string("CreateDynamicObject(").length());
     line.erase(line.find_last_of(")"), line.length());
 
     unsigned int modelid;
     float posX, posY, posZ, rotX, rotY, rotZ;
-    int succCode = sscanf(line.c_str(), "%u,%f,%f,%f,%f,%f,%f", &modelid, &posX, &posY, &posZ, &rotX, &rotY, &rotZ);
+    int succCode = sscanf(line.c_str(), "%d,%f,%f,%f,%f,%f,%f", &modelid, &posX, &posY, &posZ, &rotX, &rotY, &rotZ);
 
     if (succCode == 7)
     {
         requiredStreamer=true;
-        int obj = CreateObject(modelid, posX, posY, posZ, rotX, rotY, rotZ, drawDistance);
-        objects.push_back(obj);
+        //int obj = CreateObject(modelid, posX, posY, posZ, rotX, rotY, rotZ, drawDistance);
+        AMX_NATIVE native=sampgdk_FindNative("CreateDynamicObject");
+        if(native!=NULL)
+        {
+            int obj=sampgdk_InvokeNative(native,"dffffff")
+            objects.push_back(obj);
+        } else
+            logprintf("  Unable to load object! Required streamer");
     }
     else
-        logprintf("  Error: Object %d has wrong format",index);
+        logprintf("  Error: Object %d has wrong format",index);*/
 }
